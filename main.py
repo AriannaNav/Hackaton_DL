31c1b12273661843551bae72266c2c19634f8fc4
Arianna Navarra <ariannanavarra@Mac.Home>
2025-05-28 18:20:16 +0200

    update dimensioni main

--- main.py
+++ main.py
@@ -25,7 +25,8 @@ def main(args):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     test_set_name = args.test_path.split("/")[-2]
 
-    input_dim = 20  
+    sample_graph = train_dataset[0] if train_dataset else test_dataset[0]
+    input_dim = sample_graph.x.shape[1]
     hidden_dim = 64
     output_dim = 6
     model = ImprovedNNConv(input_dim, hidden_dim, output_dim).to(device)
