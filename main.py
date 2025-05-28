00c96f4c4984cf0a3f7b59868f74dd98a42860be
Arianna Navarra <ariannanavarra@Mac.Home>
2025-05-28 17:53:56 +0200

    update main

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def main(args):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     test_set_name = args.test_path.split("/")[-2]
 
-    input_dim = 5  # Aggiornato da 4 a 5 per includere la nuova feature 'ones'
+    input_dim = 6  # Aggiornato da 4 a 5 per includere la nuova feature 'ones'
     hidden_dim = 64
     output_dim = 6
     model = ImprovedNNConv(input_dim, hidden_dim, output_dim).to(device)
@@ -111,4 +111,3 @@ if __name__ == "__main__":
     parser.add_argument("--epochs", type=int, default=20)
     args = parser.parse_args()
     main(args)
-    
